93ffc7c5f976d44e78cf7abaaff9c032fdadbf23
cesarvargasL <cesarluisvargaslezano@gmail.com>
2023-10-12 16:33:37 -0400

    updates

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
         let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
         
         let ubicacion = document.getElementById('ubicacion')  
-        //let iconoAnimado = document.getElementById('icono-animado') 
+        let iconoAnimado = document.getElementById('icono-animado') 
     
         let vientoVelocidad = document.getElementById('viento-velocidad') 
     
@@ -21,7 +21,7 @@ window.addEventListener('load', () => {
                //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${AQUI_VIENE_TU_API_KEY}`
     
                //ubicación por ciudad
-               const url = `https://api.openweathermap.org/data/2.5/weather?q=Bogota,US&lang=es&units=metric&appid=c83f26f0e4757dff05ca66433d246020`
+               const url = `https://api.openweathermap.org/data/2.5/weather?q=Sucre, Bo&lang=es&units=metric&appid=c83f26f0e4757dff05ca66433d246020`
     
                //console.log(url)
     
@@ -37,51 +37,57 @@ window.addEventListener('load', () => {
                     //console.log(data.weather[0].description)
                     let desc = data.weather[0].description
                     temperaturaDescripcion.textContent = desc.toUpperCase()
-                    ubicacion.textContent = data.name
+                    ubicacion.textContent = data.name + "," +data.sys.country;
                     
                     vientoVelocidad.textContent = `${data.wind.speed} m/s`
                     
                     //para iconos estáticos
-                    //const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`                     
-                    //icono.src = urlIcon
-                    //console.log(data.weather[0].icon)
+                    console.log(data.weather[0].icon)
+                    let iconCode = data.weather[0].icon
+                    const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`;
+                    let icono = document.getElementById('icono-animado');
+                    icono.src = urlIcon;
+                    console.log(urlIcon)
     
                     //para iconos dinámicos
-                    console.log(data.weather[0].main)
-                    switch (data.weather[0].main) {
-                        case 'Thunderstorm':
-                          iconoAnimado.src='animated/thunder.svg'
-                          console.log('TORMENTA');
-                          break;
-                        case 'Drizzle':
-                          iconoAnimado.src='animated/rainy-2.svg'
-                          console.log('LLOVIZNA');
-                          break;
-                        case 'Rain':
-                          iconoAnimado.src='animated/rainy-7.svg'
-                          console.log('LLUVIA');
-                          break;
-                        case 'Snow':
-                          iconoAnimado.src='animated/snowy-6.svg'
-                            console.log('NIEVE');
-                          break;                        
-                        case 'Clear':
-                            iconoAnimado.src='animated/day.svg'
-                            console.log('LIMPIO');
-                          break;
-                        case 'Atmosphere':
-                          iconoAnimado.src='animated/weather.svg'
-                            console.log('ATMOSFERA');
-                            break;  
-                        case 'Clouds':
-                            iconoAnimado.src='animated/cloudy-day-1.svg'
-                            console.log('NUBES');
-                            break;  
-                        default:
-                          iconoAnimado.src='animated/cloudy-day-1.svg'
-                          console.log('por defecto');
-                      }
-    
+                    /*
+                       console.log(data.weather[0].main)
+                switch (data.weather[0].main) {
+                    case 'Thunderstorm':
+                      iconoAnimado.src='animated/thunder.svg'
+                      console.log('TORMENTA');
+                      break;
+                    case 'Drizzle':
+                      iconoAnimado.src='animated/rainy-2.svg'
+                      console.log('LLOVIZNA');
+                      break;
+                    case 'Rain':
+                      iconoAnimado.src='animated/rainy-7.svg'
+                      console.log('LLUVIA');
+                      break;
+                    case 'Snow':
+                      iconoAnimado.src='animated/snowy-6.svg'
+                        console.log('NIEVE');
+                      break;                        
+                    case 'Clear':
+                        iconoAnimado.src='animated/day.svg'
+                        console.log('LIMPIO');
+                      break;
+                    case 'Atmosphere':
+                      iconoAnimado.src='animated/weather.svg'
+                        console.log('ATMOSFERA');
+                        break;  
+                    case 'Clouds':
+                        iconoAnimado.src='animated/cloudy-day-1.svg'
+                        console.log('NUBES');
+                        break;  
+                    default:
+                      iconoAnimado.src='animated/cloudy-day-1.svg'
+                      console.log('por defecto');
+                  }
+
+            })
+                    */
                 })
                 .catch( error => {
                     console.log(error)
